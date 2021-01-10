87587d8ff954d2c05d4bfcbe0d3f71065cfb323c
a10-01a <truongnpce150842@fpt.edu.vn>
2021-01-10 22:58:33 +0700

    small changes

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import PyQt5
-from cameraControl import CameraControl
-from faceDetect import FaceDetect
 import cv2 as cv
+from faceDetect import *
+from cameraControl import *
 
 def main():
     camctrl = CameraControl()
     frame = camctrl.getFrame()
     camctrl.stopCamera()
+
     fd = FaceDetect(frame)
     faces = fd.getFaces(frame)
     cv.imshow("aaa", faces)
